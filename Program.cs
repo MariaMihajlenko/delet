11c8fb90f5a2d5e9d06780409135d5f041169554
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-14 16:12:43 +0500

    задача №1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class LengthDemo {
+
+public static void Main()
+ {   
+Console.WriteLine("Введите трехзначное число:  ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+char[]arr = numbers.ToString().ToCharArray();
+var n = arr[1];
+Console.WriteLine(n);
+
+    }
+}
+
